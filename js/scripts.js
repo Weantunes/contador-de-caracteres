781e19fc8c03b9288d836cfe89bb654394152bff
Wagner Eduardo de Souza Antunes <duzicoo@gmail.com>
2023-02-22 20:35:42 -0300

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 function active(){
 
-    var conteudo = document.getElementById('conteudo').value;
+    var conteudo = document.querySelector('#conteudo').value;
     var qtd = {"caracteres": conteudo.length}
 
-    var addcaracteres = document.getElementById('caracteres').innerText = conteudo.length;
+    var addcaracteres = document.querySelector('#caracteres').innerText = conteudo.length;
+
+    var numPalavras = conteudo.match( /(\w+)?[a-z0-9áéíóúãõâêîôûà&](ça)?(çe)?(çi)?(ço)?(çu)?(çã)?(çõ)?(\w+)?/gi ).length;
+    document.querySelector('#palavras').innerText = numPalavras;
 
 }
