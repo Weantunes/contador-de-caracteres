96a857f578f9e2c632ae79fe88216defd3d39cd8
Wagner Eduardo de Souza Antunes <duzicoo@gmail.com>
2023-02-23 14:19:01 -0300

    add melhorias de ferramentas

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,20 @@ function active(){
     var conteudo = document.querySelector('#conteudo').value;
     var qtd = {"caracteres": conteudo.length}
 
-    var addcaracteres = document.querySelector('#caracteres').innerText = conteudo.length;
+    var numCaracteres = document.querySelector('#caracteres').innerText = conteudo.length;
 
     var numPalavras = conteudo.match( /(\w+)?[a-z0-9áéíóúãõâêîôûà&](ça)?(çe)?(çi)?(ço)?(çu)?(çã)?(çõ)?(\w+)?/gi ).length;
-    document.querySelector('#palavras').innerText = numPalavras;
+    var contPalavras = document.querySelector('#palavras').innerText = numPalavras;
+    
+    // var numEspaços = conteudo.match([' ']).length;
+    // var contEspaços = document.querySelector('#espaços').innerText = numEspaços;
 
+    // se for vazio mostra erro e apaga o conteúdo.
+    if (conteudo == 0) {
+        contPalavras = '0';
+        contEspaços = '0';
+        alert('deu certo');
+    }
+
+    alert(conteudo);
 }
